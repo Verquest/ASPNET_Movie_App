8adf9d208bf94f6afde1470c9cfa5e2dfb9f9425
Verquest <verquest1337@gmail.com>
2022-06-05 18:12:38 +0200

    init

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cwiczenia_10.Models;
+
+namespace Cwiczenia_10.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cwiczenia_10.Models.Movie> Movie { get; set; }
+    }
+}
